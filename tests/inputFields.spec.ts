ebe470838b54935ad2ca706db967841e95096e00
Pavlo Kuvshynkin <pavlokuvshynkin@gmail.com>
2024-11-09 19:43:46 +0100

    Added adjusted locator names to make it more specific.

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -9,8 +9,8 @@ test.describe('Input fields', () => {
 
     test('Test Case 1: Update pet type', async ({page}) => {
         //Creating locator for targeted table row that is going to be edited
-        const tableRow = page.locator('tbody tr')
-        await tableRow.getByRole('button',{name: "Edit"}).first().click()
+        const petTableRows = page.locator('tbody tr')
+        await petTableRows.getByRole('button',{name: "Edit"}).first().click()
         //Asserting the "Edit Pet Type" text displayed
         await expect(page.locator('h2')).toHaveText('Edit Pet Type')
         //Creating a locator for input field that be cleared and inserted a new value
@@ -22,22 +22,22 @@ test.describe('Input fields', () => {
         const updateButton = page.getByRole('button', {name: "Update"})
         await updateButton.click()
         //Asserting the value of the first row is updated
-        await expect(tableRow.locator('[id="0"]')).toHaveValue('rabbit')
+        await expect(petTableRows.locator('[id="0"]')).toHaveValue('rabbit')
         //Now going to edit the same row again to back 'cat'
-        await tableRow.getByRole('button',{name: "Edit"}).first().click()
+        await petTableRows.getByRole('button',{name: "Edit"}).first().click()
         //Clearing 'rabbit' from the field and filling with 'cat' and clicking 'Update' button
         await nameInputField.click()
         await nameInputField.clear()
         await nameInputField.fill('cat')
         await updateButton.click()
         //Then asserting the 'cat' value of the same row is updated
-        await expect(tableRow.locator('[id="0"]')).toHaveValue('cat')
+        await expect(petTableRows.locator('[id="0"]')).toHaveValue('cat')
     })
 
     test('Test Case 2: Cancel pet type update', async ({page}) => {
         //Creating locator for targeted table row that is going to be edited
-        const tableRow = page.getByRole('row', { name: 'dog' })
-        await tableRow.getByRole('button', {name: "Edit"}).click()
+        const petTypeTableRow = page.getByRole('row', { name: 'dog' })
+        await petTypeTableRow.getByRole('button', {name: "Edit"}).click()
         //Clearing the field and filling 'moose'
         const nameInputField = page.locator('#name')
         await nameInputField.click()
@@ -48,7 +48,7 @@ test.describe('Input fields', () => {
         //Clicking the 'Cancel' button
         await page.getByRole('button', {name: "Cancel"}).click()
         //Then asserting the 'dog' value is remaining for the same row
-        await expect(tableRow.locator('[id="1"]')).toHaveValue('dog')
+        await expect(petTypeTableRow.locator('[id="1"]')).toHaveValue('dog')
     });
 
     test('Test Case 3: Pet type name is required validation', async ({page}) => {
