ea14e964f835ab02c46a4828843315eed151a404
Pavlo Kuvshynkin <pavlokuvshynkin@gmail.com>
2025-09-20 10:51:22 +0200

    Intercept API response homework. I believe I set up 'petclinic/api/vets' endpoint and "responseBody" correctly but for some reason when running the test it does not shows up mocked response. Can you advice if I did a mistake or there are other issues?

--- APItests/workWithAPItest.spec.ts
+++ APItests/workWithAPItest.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import owners from '../test-data/ownersDetails.json';
+//import vets from '../test-data/vetsDetails.json';
 import { PageManager } from '../Page-Objects/pageManager';
 
 test.beforeEach(async ({ page }) => {
@@ -11,6 +12,56 @@ test.beforeEach(async ({ page }) => {
     await page.route('*/**/petclinic/api/owners/*', async route => {
         await route.fulfill({
             body: JSON.stringify(owners[0])
+        });
+    });
+
+    await page.route('*/**/petclinic/api/vets', async route => {
+        const response = await route.fetch()
+        const responseBody = await response.json()
+        responseBody.vets[5].specialties = [
+            {
+                "id": 1015,
+                "name": "radiology"
+            },
+            {
+                "id": 1016,
+                "name": "surgery"
+            },
+            {
+                "id": 1017,
+                "name": "dentistry"
+            },            {
+                "id": 1018,
+                "name": "dermatology"
+            },
+            {
+                "id": 1019,
+                "name": "ophthalmology"
+            },
+            {
+                "id": 1020,
+                "name": "nutrition"
+            },
+            {
+                "id": 1021,
+                "name": "anesthesiology"
+            },
+            {
+                "id": 1022,
+                "name": "emergency and critical care"
+            },
+            {
+                "id": 1023,
+                "name": "internal medicine"
+            },
+            {
+                "id": 1024,
+                "name": "behavior"
+            }
+        ];
+        
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
         });
     });
 });
@@ -37,3 +88,13 @@ test('Owners details validation', async ({ page }) => {
     //5. Validate number of visits for the first pet
     await expect(page.locator('.table-condensed > tr')).toHaveCount(10)
 });
+
+test('Intercept API response', async ({ page }) => {
+    await page.goto('/');
+    const pm = new PageManager(page);
+    //1. Navigate to the Veterinarians page
+    await pm.navigateTo().veterinariansPage();
+    await expect(page.getByRole('heading')).toHaveText('Veterinarians');
+    //2. Validate number of specialties for the "Sharon Jenkins" is equal to 10
+    await expect(page.getByRole('row', {name: "Sharon Jenkins"}).getByRole('cell').nth(1).locator('div')).toHaveCount(10)
+})
